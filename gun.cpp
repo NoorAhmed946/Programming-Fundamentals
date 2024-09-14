13d8df82ff1e4e4c588e53ed626218628026467e
Muhammad Noor Ahmed <muhammadnoorahmed946@gmail.com>
2024-09-14 19:22:54 +0500

    PD tasks Files

---
+++ gun.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+using namespace std ; 
+int main() 
+{   
+cout <<"    +--^---------,-------,------,--------^--,   "<<endl ;
+cout <<"     | |||||||||  '_______'      |           o "<<endl;
+cout <<"     '+--------------------------^-----------|"<<endl;  
+cout <<"       ',--------,-------,-----------------'  "<<endl;
+cout <<"         / XXXXX  /'|     /'  "<<endl ;
+cout <<"        / XXXXX  /  |    /'   "<<endl ;
+cout <<"       / XXXXX  /'------'     "<<endl ;
+cout <<"      / XXXXX  /   " <<endl; 
+cout <<"     / XXXXX  /  " <<endl ; 
+cout <<"    (________(  "<<endl ;
+cout <<"     '______'   "<<endl ;          
+
+  }
+
